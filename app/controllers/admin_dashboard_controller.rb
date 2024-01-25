e494b6eaff39ca05e29053e1fffe230aed379c82
Ido Traini <ido.traini@gmail.com>
2024-01-25 12:51:06 +0100

    dato un primo aspetto grafico ad admin-dashboard per capire bulma

--- app/controllers/admin_dashboard_controller.rb
+++ app/controllers/admin_dashboard_controller.rb
@@ -2,5 +2,7 @@ class AdminDashboardController < ApplicationController
     before_action :authenticate_admin!
 
     def index
+        @total_users = User.all 
+        #@total_association = Association.all 
     end
 end
